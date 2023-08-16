fa94380408c99f8e97450bd4cda3a1516b41558c
im-usb <udaysbaghel@gmail.com>
2023-08-16 12:45:20 +0530

    Update mode and reset features

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const lineWidth = document.querySelector("#line-width");
 const colorPicker = document.querySelector("#color");
+const modeBtn = document.querySelector("#fill-color");
+const resetBtn = document.querySelector("#reset");
 
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFill = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -21,6 +24,9 @@ function onMove(event) {
 }
 
 function onMouseDown(event) {
+  if (isFill) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
   isPainting = true;
 }
 
@@ -41,9 +47,26 @@ function onColorSelect(event) {
 }
 
 function onColorClick(event) {
-  ctx.strokeStyle = event.target.dataset.color;
-  ctx.fillStyle = event.target.dataset.color;
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  colorPicker.value = colorValue;
   //   console.log(event.target.dataset.color);
+}
+
+function onModeBtnClick() {
+  if (isFill) {
+    isFill = false;
+    modeBtn.value = "Draw";
+  } else {
+    modeBtn.value = "Fill";
+    isFill = true;
+  }
+}
+
+function onResetBtnClick() {
+  ctx.fillStyle = "#FFF";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -52,5 +75,6 @@ canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mouseleave", onMouseUp);
 lineWidth.addEventListener("change", onLineWidthUpdate);
 colorPicker.addEventListener("change", onColorSelect);
-
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeBtnClick);
+resetBtn.addEventListener("click", onResetBtnClick);
